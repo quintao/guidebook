7eeb24a41e01f06e50a0e205a9916c80268aadef
Fred Quintao <quintao@gmail.com>
2025-05-06 21:43:13 +0200

    fixes button in the modal screen

--- app/sectors/ayerne.tsx
+++ app/sectors/ayerne.tsx
@@ -17,12 +17,12 @@ export default {
         },
         access: "Depuis Champéry, prendre la direction de la Cantine de Sur Cou. De la cantine, suivre une route carrossable sur 1,2 km et parquer à son terminus (pylône du téléphérique). Puis suivre le sentier d’abord à plat sur 200 mètres puis descendre dans la forêt. Après 5 minutes de descente, suivre les cairns. La falaise se trouve à gauche du chemin pédestre, après une petite remontée (50 mètres). 15 minutes depuis le parking.",
         restaurants: "La cantine de Sur Cou toute proche est une très bonne adresse pour déguster les plats typiques de la région.",
-        long_description: "Le Pic d’Ayerne séduira les grimpeurs amateurs de solitude et de calme. Son pied se perd dans a forêt, mais après quelques mètres d’escalade l’horizon se dégage sur les Dent-du-Midi en arrière-plan.\n\nAu sommet de « Classico », vous vous tiendrez sur un véritable pic avec une vue magnifique ! Falaise discrète, elle mérite un détour, et même plus d’un si vous vous tentez « Farouche » 7c qui ne se laissera pas facilement dompter !",
+        long_description: "Le Pic d’Ayerne séduira les grimpeurs amateurs de solitude et de calme. Son pied se perd dans a forêt, mais après quelques mètres d’escalade l’horizon se dégage sur les Dent-du-Midi en arrière-plan.\n\nAu sommet de « Classico », vous vous tiendrez sur un véritable pic avec une vue magnifique ! Falaise discrète, elle mérite un détour, et même plus d’un si vous vous tentez « Farouche » 7c qui ne se laissera pas facilement dompter !\n\n**Equipement**: Bernard Liegme, Frank Berrut",
     },
     sector_pictures: [
         {
             path: require("@/assets/images/ayerne/topo.jpg"),
-            description: "Apercu general du secteur"
+            description: "Apercu general"
         },
     ],
     routes: [
@@ -31,7 +31,6 @@ export default {
             grade: "6c",
             stars: 1,
             tips: "Pas de bloc au depart",
-            setter: "Frank Berrut",
             requiped: "",
         },
         {
@@ -39,7 +38,6 @@ export default {
             grade: "6a+",
             stars: 1,
             tips: "",
-            setter: "Frank Berrut",
             requiped: "",
         },
         {
