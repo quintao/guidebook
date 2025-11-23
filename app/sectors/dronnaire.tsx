e98dfce71348f3d9237b3f17e40317807eb5ddba
Fred Quintao <quintao@gmail.com>
2025-11-23 20:36:55 +0100

    adding two new sectors

---
+++ app/sectors/dronnaire.tsx
@@ -0,0 +1,79 @@
+export default {
+    overview: {
+        name: "Dronnaire",
+        short_description: "Petit secteur avec de la cornieule",
+        main_activities: "Escalade Sportive",
+        rock: "Cornieule",
+        grades: "5a-7a+",
+        orientation: "S",
+        altitude: "1800mm",
+        latitude: 46.199008,
+        longitude: 6.827668,
+    },
+    detailed_info: {
+        parking: {
+            android: "https://maps.app.goo.gl/bAcKmjrQnVgCxirFA",
+            ios: "-"
+        },
+        access: "Monthey - Val-d'Illiez - Les Crosets - Vaillime - col des Portes du Soleil. Continuer jusqu'à l'alpage de la Dronnaire (parking un peu plus loin). 10 minutes à pied. Accès possible également depuis Morgins par le vallon de They et la buvette de la Tovassière. Parking à la buvette. 15 minutes de marche. Falaise au bord de la route carrossable. ",
+        restaurants: "Buvette de la Tovassière et Buvette et Cantine de They.",
+        long_description: "Bien entendu, à la lecture du topo, le potentiel de ce petit site est bien maigre. Mais vous aurez le rare bonheur de grimper sur un rocher particulier que l’on ne rencontre pas dans les nombreux sites du Chablais: de la cornieule avec des trous plus ou moins gros et des plats. Et tout ça dans du dévers. A combiner avec le site du col des Portes du Soleil si vous avez la journée. \n\n**Equipement**: Gilbert Perrin et Frank Berrut",
+    },
+    sector_pictures: [
+        {
+            path: require("@/assets/images/dronnaire/dronnaire.jpg"),
+            description: "Ca grimpe !"
+        },
+    ],
+    routes: [
+    {
+        id: "dronnaire_1",
+        name: "Project",
+        grade: "?",
+        stars: 1,
+    },
+    {
+        id: "dronnaire_2",
+        name: "Le friend salvateur",
+        grade: "7a+",
+        stars: 3,
+        tips: "7a+ avec le rétablissement !",
+    },
+    {
+        id: "dronnaire_3",
+        name: "Au voleur",
+        grade: "6c+",
+        stars: 2,
+    },
+    {
+        id: "dronnaire_4",
+        name: "Prise de tête",
+        grade: "6b",
+        stars: 3,
+    },
+    {
+        id: "dronnaire_5",
+        name: "Dolce vita",
+        grade: "5a",
+        stars: 1,
+    },
+    {
+        id: "dronnaire_6",
+        name: "Tuf ! Tuf !",
+        grade: "5c",
+        stars: 2,
+    },
+    {
+        id: "dronnaire_7",
+        name: "Le tufeau",
+        grade: "6a+",
+        stars: 2,
+    },
+    {
+        id: "dronnaire_8",
+        name: "Tartuffe",
+        grade: "5c",
+        stars: 1,
+    },
+  ]
+};
