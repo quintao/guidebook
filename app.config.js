d502dfd26ba82898905b80d96c0535e8f8b49ec4
Frederico Paiva Quintao <frederico.paivaquintao@doctolib.com>
2026-07-28 13:46:41 +0200

    setting up env

---
+++ app.config.js
@@ -0,0 +1,67 @@
+const googleMapsApiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+export default {
+  expo: {
+    name: "Guidebook",
+    slug: "guidebook",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/projectologo.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/projectologo.png",
+        backgroundColor: "#ffffff",
+      },
+      splash: {
+        image: "./assets/images/aititude.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.quintao.guidebook",
+      config: {
+        googleMaps: {
+          apiKey: googleMapsApiKey,
+        },
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/aititude.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      "expo-font",
+      "expo-web-browser",
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "8fb41c07-0105-46d0-aff2-8b297affa229",
+      },
+      googleMapsApiKey,
+    },
+    owner: "quintao",
+  },
+};
